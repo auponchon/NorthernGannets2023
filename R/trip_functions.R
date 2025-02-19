48c72d265c9bb00e81a3074f943b3a06fd62ee1a
Aurore Ponchon <aurore.ponchon@gmail.com>
2025-02-19 19:51:59 +0100

    Change from sero to iris colour

--- R/trip_functions.R
+++ R/trip_functions.R
@@ -115,6 +115,8 @@ trips_summary_ind<-function (dataset,colony){
                                  sex=temp$sex[1],
                                  deploy=temp$deploy[1],
                                  sero=temp$sero[1],
+                                 Yeux_colo=temp$Yeux_colo[1],
+                                 Yeux_bino=temp$Yeux_bino[1],
                                  Distmaxkm=max(temp$distmax),
                                  TripDurh=sum(temp$difftimemin)/60,
                                  maxDiffTimeh=max(temp$difftimemin)/60,
@@ -154,6 +156,8 @@ land_summary_ind<-function (dataset,colony){
                                  sex=temp$sex[1],
                                  deploy=temp$deploy[1],
                                  sero=temp$sero[1],
+                                 Yeux_colo=temp$Yeux_colo[1],
+                                 Yeux_bino=temp$Yeux_bino[1],
                                  LandDurh=sum(tempo$difftimemin)/60,
                                  maxDiffTimeh=max(tempo$difftimemin)/60,
                                  nlocs=nrow(tempo),
@@ -365,6 +369,8 @@ serol.rouzic<-read_excel(here("data","RawData","data_Rouzic_2023_sexage_AIV.xlsx
                   date,
                   sexe,
                   age_chick,
+                  Yeux_colo,
+                  Yeux_bino,
                   AIV,
                   body_weight,
                   tarsus,
@@ -378,6 +384,8 @@ serol.rouzic<-read_excel(here("data","RawData","data_Rouzic_2023_sexage_AIV.xlsx
            stage=as.factor(stage),
            sex=as.factor(sex),
            sero=as.factor(sero),
+           Yeux_colo=as.factor(Yeux_colo),
+           Yeux_bino=as.factor(Yeux_bino),
            culmen=as.numeric(culmen),
            period=as.Date(period,format="%F"),
            deploy=ifelse(period < as.Date("2023-08-01",
@@ -388,4 +396,51 @@ serol.rouzic<-read_excel(here("data","RawData","data_Rouzic_2023_sexage_AIV.xlsx
     droplevels()
 
 return(serol.rouzic)
+}
+
+######################################################################################
+## Function to summarize data by mean and standard error
+######################################################################################
+
+## Summarizes data.
+## Gives count, mean, standard deviation, standard error of the mean, and confidence interval (default 95%).
+##   data: a data frame.
+##   measurevar: the name of a column that contains the variable to be summariezed
+##   groupvars: a vector containing names of columns that contain grouping variables
+##   na.rm: a boolean that indicates whether to ignore NA's
+##   conf.interval: the percent range of the confidence interval (default is 95%)
+summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
+                      conf.interval=.95, .drop=TRUE) {
+  library(plyr)
+  
+  # New version of length which can handle NA's: if na.rm==T, don't count them
+  length2 <- function (x, na.rm=FALSE) {
+    if (na.rm) sum(!is.na(x))
+    else       length(x)
+  }
+  
+  # This does the summary. For each group's data frame, return a vector with
+  # N, mean, and sd
+  datac <- ddply(data, groupvars, .drop=.drop,
+                 .fun = function(xx, col) {
+                   c(N    = length2(xx[[col]], na.rm=na.rm),
+                     mean = mean   (xx[[col]], na.rm=na.rm),
+                     sd   = sd     (xx[[col]], na.rm=na.rm)
+                   )
+                 },
+                 measurevar
+  )
+  
+  # Rename the "mean" column    
+  datac <- rename(datac, c("mean" = measurevar))
+  
+  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+  
+  # Confidence interval multiplier for standard error
+  # Calculate t-statistic for confidence interval: 
+  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+  datac$ci <- datac$se * ciMult
+  
+  return(datac)
 }
